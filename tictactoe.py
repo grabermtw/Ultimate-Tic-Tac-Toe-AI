2c27743ed623e4346cc8dd6d5014ddf09cdd5b9c
Matt Graber <grabermtw@gmail.com>
2022-11-09 21:06:45 -0500

    added ability to visualize sub-boards that have been won

--- tictactoe.py
+++ tictactoe.py
@@ -21,16 +21,28 @@ def string_of(board):
             upper_line = ""
             lower_line = ""
             for j in range(n):
-                for i in range(n):
-                    board_position = i + n**2 * j + k * n + l * n**3
-                    upper_line += "{:<4}".format(board_position)
-                    if board[board_position] != 0:
-                        lower_line += "({}) ".format(board[board_position])
-                    else:
-                        lower_line += "    "
-                    if i < n - 1:
-                        upper_line += "|"
-                        lower_line += "|"
+                board_position = n**2 * j + k * n + l * n ** 3
+                # Handle if someone's won the whole sub-board
+                if "win" in str(board[board_position]):
+                    winner = board[board_position][0]
+                    upper_line += " "
+                    lower_line += " "
+                    upper_line += "{} ".format(winner) * int((n * 4 + n - 2) / 2)
+                    lower_line += " {}".format(winner) * int((n * 4 + n - 2) / 2)
+                    if n % 2 == 1:
+                        upper_line += " "
+                        lower_line += " "
+                else:
+                    for i in range(n):
+                        board_position = i + n**2 * j + k * n + l * n**3
+                        upper_line += "{:<4}".format(board_position)
+                        if board[board_position] != 0:
+                            lower_line += "({}) ".format(board[board_position])
+                        else:
+                            lower_line += "    "
+                        if i < n - 1:
+                            upper_line += "|"
+                            lower_line += "|"
                 if j < n - 1:
                     upper_line += "//"
                     lower_line += "//"
@@ -38,13 +50,20 @@ def string_of(board):
                     upper_line += "\n"
                     lower_line += "\n"
             board_string += upper_line
-            board_string += lower_line 
+            board_string += lower_line
             if k < n - 1:
                 for j in range(n):
-                    for i in range(n):
-                        board_string += "----"
-                        if i < n - 1:
-                            board_string += "|"
+                    board_position = i + n**2 * j + k * n + l * n**3
+                    if "win" in str(board[board_position]):
+                        winner = board[board_position][0]
+                        board_string += "  "
+                        board_string += "{} ".format(winner) * int((n * 4 + n - 2) / 2 - 1)
+                        board_string += "  " if n % 2 == 1 else " "
+                    else:
+                        for i in range(n):
+                            board_string += "----"
+                            if i < n - 1:
+                                board_string += "|"
                     if j < n - 1:
                         board_string += "//"
                     else:
@@ -66,4 +85,7 @@ def check_for_big_winner(board):
 
 n = 3
 test_board = [0] * n**4
+for i in range(n**2):
+    test_board[i] = "Owin"
+
 print(string_of(test_board))
