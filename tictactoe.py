d079006e29c1e2e978ca99be10b9174bb2d3514f
Matt Graber <grabermtw@gmail.com>
2022-11-11 15:46:58 -0500

    Added check to see if anyone won the entire game

--- tictactoe.py
+++ tictactoe.py
@@ -82,15 +82,14 @@ def get_subboard_from_index(idx):
 
 # Mark a sub-board as having been won by a player
 def win_subboard(board, subidx, player):
-    print(subidx)
     for i in range(subidx, subidx + n**2):
         board[i] = "{}win".format(player)
     return board
 
 # check if the specified indices all contain the same thing
-def check_subboard_line(board, indices):
+def check_line_for_win(board, indices):
     for i in range(len(indices)):
-        if board[indices[i]] != board[indices[0]]:
+        if board[indices[i]] == 0 or board[indices[i]] != board[indices[0]]:
             return False
     return True
 
@@ -119,7 +118,7 @@ def check_for_small_winner(board, move):
     idx_lists_to_check.append(diag2_lst)
     # check each potential win
     for line in idx_lists_to_check:
-        if check_subboard_line(board, line):
+        if check_line_for_win(board, line):
             # update the board to reflect the win
             board = win_subboard(board, subidx, board[move])
             return True, board
@@ -127,13 +126,39 @@ def check_for_small_winner(board, move):
 
 # check if anyone has won the entire board
 def check_for_big_winner(board):
-    # TODO
-    return None
+    idx_lists_to_check = []
+    diag1_lst = []
+    diag2_lst = []
+    for i in range(n):
+        # horizontal and vertical
+        hor_lst = []
+        vert_lst = []
+        for j in range(n):
+            hor_lst.append(i * n**3 + j * n**2)
+            vert_lst.append(j * n**3 + i * n**2)
+        idx_lists_to_check.append(hor_lst)
+        idx_lists_to_check.append(vert_lst)
+        # diagonal
+        diag1_lst.append(n**3 * i + n**2 * i)
+        diag2_lst.append(n**3 * (n - (i + 1)) + n**2 * i)
+    idx_lists_to_check.append(diag1_lst)
+    idx_lists_to_check.append(diag2_lst)
+    # check each potential win
+    for line in idx_lists_to_check:
+        if check_line_for_win(board, line):
+            return True, board[line[0]]
+    return False, None
 
 n = 3
 test_board = [0] * n**4
 for i in range(51,54):
     test_board[i] = "O"
 _, test_board = check_for_small_winner(test_board, 53)
-
+for i in range(39,42):
+    test_board[i] = "O"
+_, test_board = check_for_small_winner(test_board, 41)
+for i in range(33,36):
+    test_board[i] = "O"
+_, test_board = check_for_small_winner(test_board, 35)
+print(check_for_big_winner(test_board))
 print(string_of(test_board))
